9317788d9c89e3d92612e91e6c3fec52e38d1a80
Andrea Esposito <espoand@tiscali.it>
2019-10-11 10:09:06 +0200

    Registration form

--- TaxiShareity/create_table.sql
+++ TaxiShareity/create_table.sql
@@ -2,8 +2,14 @@ create table users(
   username varchar(255) PRIMARY KEY,
   email varchar(255) UNIQUE,
   password varchar(255),
-  name varchar(255)
+  name varchar(255),
+  surname varchar(255),
+  contact_no varchar(255) UNIQUE ,
+  user_type ENUM('STUDENT','DRIVER') NOT NULL
+  ;
+
 
 );
 
 insert into users(username, email, password,name) values ('espoand','aaa@bbb.com','helloWorld','Andrea');
+
