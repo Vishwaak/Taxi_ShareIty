919e2a32927274dc7a9e0853b998956348834571
Haram <52073199+haram-dev@users.noreply.github.com>
2020-08-26 06:47:05 +0900

    added updates on BookRide.java (#24)
    - added a field to represent a primary key of data
    - combined date and time in a field
    - added some codes and dependencies to validate arguments in constructor
    - added Builder class
    
    Co-authored-by: Xerous Wazler <41968785+Vishwaak@users.noreply.github.com>

--- TaxiShareity/src/main/java/minewine/taxishareity/persistance/dbo/BookRide.java
+++ TaxiShareity/src/main/java/minewine/taxishareity/persistance/dbo/BookRide.java
@@ -5,64 +5,131 @@
  */
 package minewine.taxishareity.persistance.dbo;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+import static java.time.LocalDateTime.now;
+import static org.apache.commons.lang3.ObjectUtils.defaultIfNull;
+
 /**
  *
  * @author xerous
  */
 public class BookRide {
-    private String pickup_loc;
-    private String drop_loc;
-    private String No_people;
-    private String time;
-    private String date;
 
-    
-    public BookRide(String pickup_loc,String drop_loc,String No_people,String time,String date){
-        
-        this.pickup_loc = pickup_loc;
-        this.drop_loc = drop_loc;
-        this.No_people = No_people;
-        this.time = time;
-        this.date = date;
-    }
-    public String getDate() {
-        return date;
-    }
+    private final Long seq;
+    private String pickupLoc;
+    private String dropLoc;
+    private Integer peopleNum;
+    private final LocalDateTime createAt;
 
-    public void setDate(String date) {
-        this.date = date;
-    }
+    // specify the minimum number of people to book ride
+    public static final Integer MIN_NUMBER_OF_PEOPLE = 2;
 
-    public String getPickup_loc() {
-        return pickup_loc;
+    public BookRide(Long seq, String pickupLoc, String dropLoc, Integer peopleNum, LocalDateTime createAt) {
+        checkNotNull(seq, "seq must be provided.");
+        checkNotNull(pickupLoc, "pickupLoc must be provided.");
+        checkNotNull(dropLoc, "dropLoc must be provided.");
+        checkArgument(peopleNum > MIN_NUMBER_OF_PEOPLE,
+                String.format("peopleNum must be over %d.", MIN_NUMBER_OF_PEOPLE));
+
+        this.seq = seq;
+        this.pickupLoc = pickupLoc;
+        this.dropLoc = dropLoc;
+        this.peopleNum = peopleNum;
+        this.createAt = defaultIfNull(createAt, now());
     }
 
-    public void setPickup_loc(String pickup_loc) {
-        this.pickup_loc = pickup_loc;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BookRide bookRide = (BookRide) o;
+        return seq.equals(bookRide.seq);
     }
 
-    public String getDrop_loc() {
-        return drop_loc;
+    @Override
+    public int hashCode() {
+        return Objects.hash(seq);
     }
 
-    public void setDrop_loc(String drop_loc) {
-        this.drop_loc = drop_loc;
+    @Override
+    public String toString() {
+        return "BookRide{" +
+                "seq=" + seq +
+                ", pickupLoc='" + pickupLoc + '\'' +
+                ", dropLoc='" + dropLoc + '\'' +
+                ", peopleNum=" + peopleNum +
+                ", createAt=" + createAt +
+                '}';
     }
 
-    public String getNo_people() {
-        return No_people;
+    public Long getSeq() {
+        return seq;
     }
 
-    public void setNo_people(String No_people) {
-        this.No_people = No_people;
+    public String getPickupLoc() {
+        return pickupLoc;
     }
 
-    public String getTime() {
-        return time;
+    public String getDropLoc() {
+        return dropLoc;
     }
 
-    public void setTime(String time) {
-        this.time = time;
+    public Integer getpeopleNum() {
+        return peopleNum;
     }
-      
+
+    public LocalDateTime getCreateAt() {
+        return createAt;
+    }
+
+    public static class Builder {
+        private Long seq;
+        private String pickupLoc;
+        private String dropLoc;
+        private Integer peopleNum;
+        private LocalDateTime createAt;
+
+        public Builder() {}
+
+        public Builder(BookRide bookRide) {
+            this.seq = bookRide.seq;
+            this.pickupLoc = bookRide.pickupLoc;
+            this.dropLoc = bookRide.dropLoc;
+            this.peopleNum = bookRide.peopleNum;
+            this.createAt = bookRide.createAt;
+        }
+
+        public Builder seq(Long seq) {
+            this.seq = seq;
+            return this;
+        }
+
+        public Builder pickupLoc(String pickupLoc) {
+            this.pickupLoc = pickupLoc;
+            return this;
+        }
+
+        public Builder dropLog(String dropLoc) {
+            this.dropLoc = dropLoc;
+            return this;
+        }
+
+        public Builder peopleNum(Integer peopleNum) {
+            this.peopleNum = peopleNum;
+            return this;
+        }
+
+        public Builder createAt(LocalDateTime createAt) {
+            this.createAt = createAt;
+            return this;
+        }
+
+        public BookRide build(){
+            return new BookRide(seq, pickupLoc, dropLoc, peopleNum, createAt);
+        }
+    }
 }
